214893bcbca0ac382870a70f96cbb6ab283df69e
C00288136 <c00288136@setu.ie>
2024-01-13 20:47:15 +0000

    Day 17 finished

---
+++ Quiz_Project/main.py
@@ -0,0 +1,16 @@
+from question_model import question
+from data import question_data
+from quiz_brain import Quizbrain
+question_bank = []
+for item in question_data:
+    question_text = item["question"]
+    question_answer = item["correct_answer"]
+    new_question = question(question_text,question_answer)
+    question_bank.append(new_question)
+    
+quiz = Quizbrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+    
+print("You completed the quiz")
+print(f"Your final score was {quiz.score}/{len(quiz.question_list)}")
