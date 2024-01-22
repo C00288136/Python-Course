afaaf3e9b050234240ba76442908045088abdee9
C00288136 <C00288136@setu.ie>
2024-01-22 20:54:02 +0000

    make snake controls

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,23 +1,22 @@
-from turtle import Screen,Turtle
+from turtle import Screen, Turtle
+import time
+from snake import Snake
 
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
-
-snake_parts = [1,2,3]
-start_position = [0,-20,-40]
-for part in range(0,3):
-    snake_part = Turtle(shape="square")
-    snake_part.color("white")
-    snake_part.goto(start_position[part],0)
+screen.tracer(0)
 
 
-    snake_parts.append(part)
+snake = Snake()
 
-
-
-
-
+game_on = True
+while game_on:
+    screen.update()
+    # screen update make it so the graphics of the snake update, and they piece don't move individually 1 by 1
+    time.sleep(0.1)
+    snake.move()
+    # because the 3 pieces of the snake are not connected we need this method to make sure that the snake looks as if
+    # it is connected the last piece is moved into the position of the second last which keeps the flow
 screen.exitonclick()
-
---
+++ Snake_game/snake.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+# Snake class
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DIS = 20
+
+
+class Snake:
+    def __init__(self):
+        self.snake_parts = []
+        self.create_snake()
+
+    def create_snake(self):
+        self.snake_parts = []
+        for position in STARTING_POSITIONS:
+            new_part = Turtle('square')
+            new_part.color('white')
+            new_part.penup()
+            new_part.goto(position)
+            self.snake_parts.append(new_part)
+
+    def move(self):
+        # because the 3 pieces of the snake are not connected we need this method to make sure that the snake looks
+        # as if it is connected the last piece is moved into the position of the second last which keeps the flow
+
+        for segment_num in range(len(self.snake_parts) - 1, 0, -1):
+            new_x = self.snake_parts[segment_num - 1].xcor()
+            new_y = self.snake_parts[segment_num - 1].ycor()
+            self.snake_parts[segment_num].goto(new_x, new_y)
+        self.snake_parts[0].forward(MOVE_DIS)
