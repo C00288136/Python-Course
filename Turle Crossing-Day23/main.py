cabf6cd5acae8555dc0a117dec5b5022c9a81615
C00288136 <C00288136@setu.ie>
2024-02-01 20:49:12 +0000

    crossy turtle complete

--- Turle Crossing-Day23/main.py
+++ Turle Crossing-Day23/main.py
@@ -7,8 +7,32 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    screen.listen()
+    screen.onkey(player.move,'Up')
+
+    car_manager.create_cars()
+    car_manager.move_car()
+
+#     detect collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+#     detec finish line
+    if player.is_at_finishline():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.levelup()
+
+
+screen.exitonclick()
--- Turle Crossing-Day23/player.py
+++ Turle Crossing-Day23/player.py
@@ -1,7 +1,28 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('black')
+        self.shape("turtle")
+        self.lt(90)
+        self.go_to_start()
+        self.penup()
+
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.teleport(STARTING_POSITION[0],STARTING_POSITION[1])
+
+    def is_at_finishline(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
