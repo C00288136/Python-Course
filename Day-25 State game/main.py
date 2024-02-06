e874f1f7ed1ccd7b06700f1d85fa5349ea29f611
C00288136 <c00288136@setu.ie>
2024-02-06 21:24:00 +0000

    creating csv files and working with panda lib

--- Day-25 State game/main.py
+++ Day-25 State game/main.py
@@ -5,16 +5,51 @@
 #
 # print(data)
 
+#
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+# #     this creates a csv object
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#         # prints all the data from row one which is temps into a array
+#     # print(temperatures)
 
-import csv
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-#     this creates a csv object
-    temperatures = []
-    for row in data:
-        temperatures.append(int(row[1]))
-        # prints all the data from row one which is temps into a array
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
 
 
+# data_dict = data.to_dict()
+# # print(data_dict)
+#
+# temp_list = data["temp"].tolist()
+# # print(temp_list)
+# total = 0
+
+# print(data["temp"].max())
+
+# print(data.condition)
+
+# get data in row
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(monday.temp)
+# print((monday.temp * 9/5) + 32)
+
+
+#Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy","Ryan"],
+    "scores" : [65,23]
+}
+
+# used to create a csv from panda library
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
