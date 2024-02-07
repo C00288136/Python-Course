662005f4d8578fe70a40e262950c28a8f058a7a5
C00288136 <c00288136@setu.ie>
2024-02-07 10:08:50 +0000

    working with central park squirrel csv

--- Day-25 State game/main.py
+++ Day-25 State game/main.py
@@ -17,12 +17,12 @@
 #             temperatures.append(int(row[1]))
 #         # prints all the data from row one which is temps into a array
 #     # print(temperatures)
-
-import pandas
-
-data = pandas.read_csv("weather_data.csv")
-# print(data["temp"])
-
+#
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# # print(data["temp"])
+#
 
 # data_dict = data.to_dict()
 # # print(data_dict)
@@ -42,14 +42,45 @@ data = pandas.read_csv("weather_data.csv")
 # print(monday.temp)
 # print((monday.temp * 9/5) + 32)
 
-
-#Create a dataframe from scratch
-data_dict = {
-    "students": ["Amy","Ryan"],
-    "scores" : [65,23]
-}
+#
+# #Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy","Ryan"],
+#     "scores" : [65,23]
+# }
 
 # used to create a csv from panda library
-data = pandas.DataFrame(data_dict)
-print(data)
-data.to_csv("new_data.csv")
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240207.csv")
+red = 0
+grey = 0
+black = 0
+
+
+# grey_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+# can use this loop or this code above to access all the data from the csv
+
+
+for color in data["Primary Fur Color"]:
+    if color == "Cinnamon":
+        red +=1
+    elif color == "Gray":
+        grey +=1
+    else:
+        black +=1
+
+print(data["Primary Fur Color"])
+data_dict = {
+    "Fur color": ["Grey","Cinnamon","Black"],
+    "Count": [grey,red,black]
+}
+new_data = pandas.DataFrame(data_dict)
+new_data.to_csv("Squirrel_Colors.csv")
